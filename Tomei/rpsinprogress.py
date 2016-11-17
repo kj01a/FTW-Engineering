a1313f2300e5b021d1ca9bd505fcc4eaee3d5902
Kevin Welch <kj01a@yahoo.com>
2016-11-17 10:05:56 -0600

    Create rpsinprogress.py

---
+++ Tomei/rpsinprogress.py
@@ -0,0 +1,6 @@
+print "Hello, it is Rock, Paper, Scissors time!"
+print "Please choose Rock, Paper, or Scissors."
+player_one_choice = raw_input()
+print "You choose %s!" % player_one_choice
+2 Comments
+
