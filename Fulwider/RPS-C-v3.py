aa516b987a37aa9630e7841a6a8819699b5c8621
lordoftheWARr3n <mrfulwider@gmail.com>
2016-12-26 00:40:39 -0500

    Create RPS-C-v3.py
    I reviewed my script and found I was mostly to standard. 
    
    Changed the alignment of delimiters in line 6.

---
+++ Fulwider/RPS-C-v3.py
@@ -0,0 +1,62 @@
+##Revised to meet PEP8 standards
+import random
+
+weapons = ["rock","paper","scissor"]
+
+result = {
+    1 : "Rock beats Scissors! You win!",
+    2 : "Rock loses to Paper! You lost!",
+    3 : "Scissors beats Paper! You win!",
+    4 : "Scissors loses to Rock! You lose!",
+    5 : "Paper beats Rock! You win!",
+    6 : "Paper loses to Scissors! You lose!",
+    7 : "It's a tie!"
+    }
+
+def weapon():
+    print """
+    Welcome to Rock, Paper, Scissors!
+    Be prepared for battle. First you
+    shall choose a weapon for battle.
+    Then the computer will choose and
+    battle begins.
+
+    Choose your weapon....
+    """
+    choice = raw_input("...")
+    if choice in weapons:
+        return choice
+    else:
+        print "There is no weapon of that kind here. Try again."
+        weapon()
+
+def battle(passed):
+    print passed
+    enemy = random.choice(weapons)
+    print "Your opponent chooses " + enemy
+    if passed == "rock":
+        if enemy == "rock":
+            print "Again slaves!"
+        elif enemy == "paper":
+            print "You Lose!"
+        else:
+            print "you win!"
+    elif passed == "paper":
+        if enemy == "paper":
+            print "Again slaves!"
+        elif enemy == "scissor":
+            print "You Lose!"
+        else:
+            print "you win! but you're a pussy for choosing paper"
+    elif passed == "scissor":
+        if enemy == "scissor":
+            print "Again slaves!"
+        elif enemy == "rock":
+            print "You Lose!"
+        else:
+            print "you win!"
+    else:
+        print "error"
+
+foo=weapon()
+battle(foo)
