74837d30fbacf3038d92396cfe8876d0a3f63cc6
Kevin Welch <john.prester01@gmail.com>
2017-01-05 09:04:15 -0600

    Add files via upload

--- Welch/rps.py
+++ Welch/rps.py
@@ -1,5 +1,8 @@
+#CHANGELOG
 #Rewrote, made cleaner, need to comment whole thing...
+#1.2 migrated to python 3.6
 
+#Rock, Paper, Scissors: The Game v 1.2
 from random import randint
 from sys import exit
 
@@ -7,15 +10,15 @@ players = [
 		"rock",
 		"paper",
 		"scissors"
-	]	
+	]
 opponent = players[randint(0, 2)]
 result = {
 		1 : "Your opponent threw Scissors! Rock beats Scissors! You win!",
 		2 : "Your opponent threw Paper! Rock loses to Paper! You lost!",
-		3 : "Your opponent threw Paper! Scissors beats Paper! You win!", 
+		3 : "Your opponent threw Paper! Scissors beats Paper! You win!",
 		4 : "Your opponent threw Rock! Scissors loses to Rock! You lose!",
 		5 : "Your opponent threw Rock! Paper beats Rock! You win!",
-		6 : "Your opponent threw Scissors! Paper loses to Scissors! You lose!", 
+		6 : "Your opponent threw Scissors! Paper loses to Scissors! You lose!",
 		7 : "You're opponent chose the same! It's a tie!"
 	}
 
@@ -25,38 +28,38 @@ def game(opponent):
 	print("Rock,")
 	print("Paper,")
 	print("or Scissors!")
-	
-	choice = raw_input("> ")
-	
+
+	choice = input("> ")
+
 	if choice.lower() == "rock":
 		if opponent == "scissors":
-			print result[1]
+			print(result[1])
 			return try_again()
 		elif opponent == "paper":
-			print result[2]
+			print(result[2])
 			return try_again()
 		else:
-			print result[7]
+			print(result[7])
 			return try_again()
 	elif choice.lower() == "scissors":
 		if opponent == "paper":
-			print result[3]
+			print(result[3])
 			return try_again()
 		elif opponent == "rock":
-			print result[4]
+			print(result[4])
 			return try_again()
 		else:
-			print result[7]
+			print(result[7])
 			return try_again()
 	elif choice.lower() == "paper":
 		if opponent == "rock":
-			print result[5]
+			print(result[5])
 			return try_again()
 		elif opponent == "scissors":
-			print result[6]
+			print(result[6])
 			return try_again()
 		else:
-			print result[7]
+			print(result[7])
 			return try_again()
 	else:
 		print("That is not an option. Please try again.")
@@ -65,9 +68,9 @@ def game(opponent):
 def try_again():
 	print("\nWasn't that fun!?")
 	print("Would you like to try again?")
-	
-	try_again = raw_input("> ")
-	
+
+	try_again = input("> ")
+
 	if try_again.lower() == "yes":
 		opponent = players[randint(0,2)]
 		return game(opponent)
@@ -75,5 +78,5 @@ def try_again():
 		exit()
 
 
-		
+
 game(opponent)
