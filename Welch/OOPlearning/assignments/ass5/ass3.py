226c6ba4a3eae9755bc677b0b310fe634da2f7ee
kj01a <john.prester01@gmail.com>
2017-03-09 21:52:14 -0600

    last parts I did of the OOPlessons

--- Welch/OOPlearning/assignments/ass5/ass3.py
+++ Welch/OOPlearning/assignments/ass5/ass3.py
@@ -1,11 +1,11 @@
 from ass5 import ConfigDict
 
 
-cc = ConfigDict("config_text.json")
+cc = ConfigDict("config_file")
 
 print(cc["sql_query"])
 print(cc['email_to'])
 
 cc['database'] = 'mysql_managed'
 
-print(cc)
+#print(cc)
--- Welch/OOPlearning/assignments/ass5/ass5.py
+++ Welch/OOPlearning/assignments/ass5/ass5.py
@@ -2,8 +2,6 @@ import os
 import json
 
 
-config_directory = '/home/dblaikie/configs/'   # added:  see below
-
 class ConfigKeyError(Exception):
     def __init__(self, this, key):
         self.key = key
@@ -14,37 +12,25 @@ class ConfigKeyError(Exception):
 
 class ConfigDict(dict):
 
-    def __init__(self, filename):
-        self._filename = filename
-        if not os.path.isfile(self._filename):
-            try:
-                open(self._filename, 'w').close()
-            except IOError:
-                raise IOError('arg to ConfigDict must be a valid filename')
-        #with open(self._filename) as f:
-        #    json.load(f)
-        #    print("open file")
-            #for line in fh:
-            #    line = line.rstrip()
-            #    key, value = line.split('=', 1)
-            #dict.__setitem__(self, key, value)
+    config_directory = 'E:/FTW-Engineering/Welch/OOPlearning/assignments/ass5'
 
-    def open_the_file(self):
+    def __init__(self, filename):
+        self._filename = os.path.join(ConfigDict.config_directory, filename + '.json')
+
+        if not os.path.isfile(self._filename):
+            with open(self._filename, 'w') as f:
+                json.dump({}, f)
+
         with open(self._filename) as f:
-            self.conf = json.load(f)
+            conf = json.load(f)
+            self.update(conf)
 
     def __getitem__(self, key):
-        #if not key in self:
-        #    raise ConfigKeyError(self, key)
-        self.open_the_file()
-        return self.conf[key]
-        #with open(self._filename) as f:
-        #    conf = json.load(f)
-        #    return conf[key]
-            #return dict.__getitem__(self, key)
+        if not key in self:
+            raise ConfigKeyError(self, key)
+        return dict.__getitem__(self, key)
 
-    """def __setitem__(self, key, value):
-        #dict.__setitem__(self, key, value)
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
         with open(self._filename, 'w') as f:
-            json.dump(conf, f)
-"""
+            json.dump(self, f)
